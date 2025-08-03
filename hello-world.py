5b240e2d81c6356b25a88decab8d7625eedc020b
mineduck1608 <ducdm160804@gmail.com>
2025-08-03 16:18:59 +0700

    up

---
+++ hello-world.py
@@ -0,0 +1,5 @@
+print("Hello, world!")
+character_name = "Sáng Minh"
+is_hansome = True
+print(f"Đây là một câu chuyện nhỏ về anh {character_name} đập chai. ")
+print(is_hansome)
---
+++ string.py
@@ -0,0 +1,2 @@
+channel_name = "Tờ Mờ Sáng \nhọc lập trình"
+print(channel_name.replace("Sáng", "Tối"))
